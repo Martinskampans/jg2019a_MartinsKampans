ee5be9b7e4dcba4be22dbef717a72c904b3cbda1
marti <martins.kampan@gmail.com>
2019-10-10 23:15:20 +0300

    majasdarbs1 level2

---
+++ src/lv/javaguru/lesson2/RandomNumberServiceTest.java
@@ -0,0 +1,21 @@
+package lv.javaguru.lesson2;
+
+import java.util.Random;
+
+public class RandomNumberServiceTest {
+    public static void main(String[] args) {
+        Random randomGenerator = new Random();
+        int rundomNumber1 = randomGenerator.nextInt(100);
+        int rundomNumber2 = randomGenerator.nextInt(100);
+        int rundomNumber3 = randomGenerator.nextInt(100);
+        int sum;
+
+        System.out.println("First number is: " + rundomNumber1);
+        System.out.println("Second number is: " + rundomNumber2);
+        System.out.println("Third number is: " + rundomNumber3);
+
+        sum = rundomNumber1 + rundomNumber2 + rundomNumber3;
+
+        System.out.println("Sum of all numbers is: " + sum);
+    }
+}
