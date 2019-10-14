eb2bc13124ed517fa29e61a08bbd5e74823bcd13
marti <martins.kampan@gmail.com>
2019-10-14 22:53:49 +0300

    majasdarbs2 level 1

---
+++ src/lv/javaguru/lesson3/car/CarTest.java
@@ -0,0 +1,13 @@
+package lv.javaguru.lesson3.car;
+
+public class CarTest {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setColor("red");
+        car1.setManufacturer("Ford");
+        car1.setYear(1965);
+
+        System.out.println("This " + car1.getColor() + " " + car1.getManufacturer() + " is produced in "
+                + car1.getYear() + ".");
+    }
+}
